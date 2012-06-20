2183aaa43df89c2d7fa092e2a4ba1bc8ad97a36d
mcintyre321 <mcintyre321@gmail.com>
2012-06-20 21:53:57 +0100

    Made Fragment rule pluggable

--- Walkies/FragmentExtension.cs
+++ Walkies/FragmentExtension.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Runtime.CompilerServices;
 
 namespace Walkies
 {
     public static class FragmentExtension
     {
+        static FragmentExtension()
+        {
+            GetFragmentRule = obj => fragments.GetValue(obj, c => null);
+            SetFragmentRule = (obj, fragment) =>
+            {
+                fragments.Remove(obj);
+                fragments.Add(obj, fragment);
+            };
+        }
         private static readonly ConditionalWeakTable<object, string> fragments = new ConditionalWeakTable<object, string>();
 
+        public static Action<object, string> SetFragmentRule { get; set; }
         public static T SetFragment<T>(this T obj, string fragment)
         {
-            fragments.Remove(obj);
-            fragments.Add(obj, fragment);
+            SetFragmentRule(obj, fragment);
             return obj;
         }
 
+        public static Func<string, object> GetFragmentRule { get; set; }
         public static string GetFragment(this object obj)
         {
             return fragments.GetValue(obj, c => null);
